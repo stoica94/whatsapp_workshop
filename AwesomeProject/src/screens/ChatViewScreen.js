e0016dbb52f81b2968bcaa299487605b3204ece2
stoica94 <ovytkl@gmail.com>
2019-10-23 17:04:40 +0100

    Added graphQL

--- AwesomeProject/src/screens/ChatViewScreen.js
+++ AwesomeProject/src/screens/ChatViewScreen.js
@@ -7,17 +7,82 @@ import {
   StyleSheet,
   KeyboardAvoidingView,
   Platform,
+  ActivityIndicator,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import {getMessagesById, postMessage} from '../services/api';
 import Compose from '../components/Compose';
 import Message from '../components/Message';
+import {useSubscription, useMutation} from '@apollo/react-hooks';
+import {gql} from 'apollo-boost';
 
-const ChatViewScreen = () => {
-  const keyboardVerticalOffset = Platform.OS === 'ios' ? 90 : 0;
+const SEND_MESSAGE = gql`
+  mutation sendMessage($userId: uuid, $conversationId: uuid, $text: String) {
+    insert_chats(
+      objects: {
+        userId: $userId
+        conversationId: $conversationId
+        message: $text
+      }
+    ) {
+      affected_rows
+    }
+  }
+`;
 
-  const renderMessageItem = item => {
-    return <Message {...item} />;
+const GET_CHATS = gql`
+  subscription getChats($conversationId: uuid) {
+    chats(where: {conversationId: {_eq: $conversationId}}) {
+      id
+      message
+      date
+      userId
+    }
+  }
+`;
+
+const ChatViewScreen = ({navigation}) => {
+  const {loading, error, data} = useSubscription(GET_CHATS, {
+    variables: {
+      conversationId: navigation.state.params.id,
+    },
+  });
+
+  const [sendMessage] = useMutation(SEND_MESSAGE);
+
+  const postMessage = text => {
+    sendMessage({
+      variables: {
+        userId: navigation.state.params.userId,
+        conversationId: navigation.state.params.id,
+        text,
+      },
+      refetchQueries: ['getChats'],
+    });
   };
+
+  const getChatsView = () => {
+    if (loading) {
+      return <ActivityIndicator />;
+    }
+    if (error) {
+      return (
+        <View>
+          <Text>{JSON.stringify(error)}</Text>
+        </View>
+      );
+    }
+
+    return (
+      <FlatList
+        style={styles.container}
+        data={data.chats}
+        renderItem={Message}
+        keyExtractor={item => `message-${item.id}`}
+      />
+    );
+  };
+  const keyboardVerticalOffset = Platform.OS === 'ios' ? 90 : 0;
 
   const [messages, setMessages] = useState([]);
   useEffect(() => {
@@ -34,16 +99,11 @@ const ChatViewScreen = () => {
     <ImageBackground
       style={styles.container}
       source={require('../assets/chat-background.jpg')}>
-      <FlatList
-        style={styles.container}
-        data={messages}
-        renderItem={({item}) => renderMessageItem(item)}
-        keyExtractor={(item, index) => `message-${index}`}
-      />
+      {getChatsView()}
       <KeyboardAvoidingView
         behavior={Platform.OS === 'ios' ? 'padding' : null}
         keyboardVerticalOffset={keyboardVerticalOffset}>
-        <Compose submit={submit} style={styles.compose} />
+        <Compose submit={postMessage} style={styles.compose} />
       </KeyboardAvoidingView>
     </ImageBackground>
   );
@@ -52,9 +112,16 @@ const ChatViewScreen = () => {
 ChatViewScreen.navigationOptions = ({navigation}) => {
   return {
     title: navigation.state.params.title,
+    headerLeft: (
+      <Icon
+        name="chevron-left"
+        size={40}
+        color="#ffffff"
+        onPress={() => navigation.goBack()}
+      />
+    ),
   };
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- AwesomeProject/src/screens/Conversations.js
+++ AwesomeProject/src/screens/Conversations.js
@@ -1,30 +1,57 @@
-import {View, StyleSheet, FlatList} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  FlatList,
+  Text,
+  ActivityIndicator,
+} from 'react-native';
 import React, {useState, useEffect} from 'react';
 import {getChats} from '../services/api';
 import ChatItem from '../components/ChatItem';
+import {useQuery} from '@apollo/react-hooks';
+import {gql} from 'apollo-boost';
+
+const GET_CONVERSATIONS = gql`
+  {
+    conversations {
+      id
+      title
+      description
+      date
+      user {
+        id
+        avatar
+      }
+    }
+  }
+`;
 
 const ChatView = ({navigation}) => {
-  const [currentChats, setCurrentChats] = useState([]);
-  useEffect(() => {
-    getChats().then(responseChats => setCurrentChats(responseChats));
-  }, []);
+  const {loading, error, data} = useQuery(GET_CONVERSATIONS);
 
-  const renderChatItem = ({item}) => {
+  const getConversationsView = () => {
+    if (loading) {
+      return <ActivityIndicator />;
+    }
+    if (error) {
+      return (
+        <View>
+          <Text>{JSON.stringify(error)}</Text>
+        </View>
+      );
+    }
     return (
-      <ChatItem {...item} navigate={navigation.navigate} date={'Yesterday'} />
-    );
-  };
-
-  return (
-    <View style={styles.container}>
       <FlatList
-        renderItem={renderChatItem}
-        keyExtractor={item => item.id}
-        data={currentChats}
+        data={data.conversations}
+        renderItem={({item}) => (
+          <ChatItem item={item} navigate={navigation.navigate} />
+        )}
+        keyExtractor={(item, index) => `message-${index}`}
         ItemSeparatorComponent={() => <View style={styles.separator} />}
       />
-    </View>
-  );
+    );
+  };
+  return <View style={styles.container}>{getConversationsView()}</View>;
 };
 
 ChatView.navigationOptions = {
